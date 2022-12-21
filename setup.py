0a2a76a131de776c8f77831df6a5e13e876b5e6b
niqinggood <niqinggood@163.com>
2022-12-21 12:38:21 +0800

    'add'

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="saddle",
+    version="0.1.0",
+    author="patrickNi",
+    author_email="niqinggood@gmail.com",
+    description="Use this package to make Machine Learn Algorithm more convenient ",
+    long_description="made by Patrick",
+    long_description_content_type="text",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
